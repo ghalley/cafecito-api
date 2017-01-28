85eb10c584b5b3309e6369daa07ea27e11336b3f
Gabriel Halley <gabriel@callrail.com>
2017-01-28 15:58:15 -0500

    Successfully return random image’s url

--- app/controllers/cafecito_controller.rb
+++ app/controllers/cafecito_controller.rb
@@ -1,6 +1,11 @@
 class CafecitoController < ApplicationController
   before_filter :authenticate
   def show
-    photos = flickr.photosets.getPhotos(photoset_id: Rails.application.secrets.cafectio_photoset_id, extras: 'tags')
+    photos = flickr.photosets.getPhotos(photoset_id: Rails.application.secrets.cafectio_photoset_id, extras: 'tags')['photo']
+
+    photo = photos.sample
+
+    url = "https://farm#{photo['farm']}.staticflickr.com/#{photo['server']}/#{photo['id']}_#{photo['secret']}.jpg"
+    render json: url.to_json
   end
 end
