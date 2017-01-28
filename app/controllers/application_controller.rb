5f72cb2812941a6a8172fbbfcd1aa03a6a3375b4
Gabriel Halley <gabriel@callrail.com>
2017-01-28 14:29:19 -0500

    setup auth method for slack token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  helper_method :authenticate
+
+  def authenticate
+    unless params['token'].present? && params['token'] == Rails.application.secrets.slack_token
+      head :forbidden
+    end
+  end
 end
