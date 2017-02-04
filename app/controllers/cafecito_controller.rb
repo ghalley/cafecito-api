857d93739adde8f683626c56b6cf29bff2498ac5
Gabriel Halley <gabriel@callrail.com>
2017-02-03 20:24:36 -0500

    Cafecito time text

--- app/controllers/cafecito_controller.rb
+++ app/controllers/cafecito_controller.rb
@@ -23,6 +23,6 @@ class CafecitoController < ApplicationController
     photo = search.present? ? search.sample : photos.sample
 
     url = "https://farm#{photo['farm']}.staticflickr.com/#{photo['server']}/#{photo['id']}_#{photo['secret']}.jpg"
-    render json: {text: url, unfurl_media: true, attachments: [{image_url: url}], unfurl_links: true}
+    render json: {text: 'Cafecito Time!', unfurl_media: true, attachments: [{image_url: url}], unfurl_links: true}
   end
 end
