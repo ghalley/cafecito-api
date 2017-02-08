7572c3b7c24001f3688a2dbd6168b1b8f175cd3e
Gabriel Halley <gabriel@callrail.com>
2017-02-08 11:32:59 -0500

    Filter weekdays, split up methods

--- app/controllers/cafecito_controller.rb
+++ app/controllers/cafecito_controller.rb
@@ -1,26 +1,21 @@
 class CafecitoController < ApplicationController
   before_filter :authenticate
   def show
-    photos = flickr.photosets.getPhotos(photoset_id: Rails.application.secrets.cafectio_photoset_id, extras: 'tags')['photo']
+    @photos = flickr.photosets.getPhotos(photoset_id: Rails.application.secrets.cafectio_photoset_id, extras: 'tags')['photo']
 
     holiday = Holiday.new
 
     if holiday.holiday?
-      search = photos.select {|p| p['tags'].include?(holiday.holiday_tag)}
+      find_tag(holiday.holiday_tag)
     elsif params[:text].present?
-      search = photos.select { |p| p['tags'].split(' ').include?(params[:text]) }
+      find_tag(params[:text])
+    elsif check_weekday
     else
-      search = []
-      photos.each do |p|
-        include_photo = true
-        p['tags'].split(' ').each do |tag|
-          include_photo = false if holiday.holiday_list.values.include?(tag)
-        end
-        search << p if include_photo
-      end
+      remove_tags(holiday.holiday_list.values)
+      @photos = @photos.select { |p| p['tags'] == 'cafecito' }
     end
 
-    photo = search.present? ? search.sample : photos.sample
+    photo = @photos.sample
 
     url = "https://farm#{photo['farm']}.staticflickr.com/#{photo['server']}/#{photo['id']}_#{photo['secret']}.jpg"
     render json: {
@@ -28,6 +23,39 @@ class CafecitoController < ApplicationController
       text: 'Cafecito Time!',
       unfurl_media: true,
       attachments: [{image_url: url}],
-      unfurl_links: true}
+      unfurl_links: true,
+      tags: photo['tags']}
+  end
+
+  private
+
+  def remove_tags(tags)
+    search = []
+    @photos.each do |p|
+      include_photo = true
+      p['tags'].split(' ').each do |tag|
+        include_photo = false if tags.include?(tag)
+      end
+      search << p if include_photo
+    end
+    @photos = search
+  end
+
+  def find_tag(tag)
+    @photos = @photos.select { |p| p['tags'].include?(tag)}
+  end
+
+  def check_weekday
+    today = Date.today
+    if today.wednesday?
+      find_tag('wednesday')
+      return true
+    elsif today.friday?
+      find_tag('friday')
+      return true
+    else
+      remove_tags(['wednesday', 'friday'])
+    end
+    false
   end
 end
