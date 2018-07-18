e07fd06ede4ca4e6d7185ea3b727998c1e30896e
Gabriel Halley <gabriel@groundfloor.us>
2018-07-18 17:07:30 -0400

    remove authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,2 @@
 class ApplicationController < ActionController::API
-  helper_method :authenticate
-
-  def authenticate
-    unless params['token'].present? && params['token'] == Rails.application.secrets.slack_token
-      head :forbidden
-    end
-  end
 end
--- app/controllers/cafecito_controller.rb
+++ app/controllers/cafecito_controller.rb
@@ -1,5 +1,4 @@
 class CafecitoController < ApplicationController
-  before_filter :authenticate
   def show
     @photos = flickr.photosets.getPhotos(photoset_id: Rails.application.secrets.cafectio_photoset_id, extras: 'tags,original_format')['photo']
 
